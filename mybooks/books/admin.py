a409d8782d2fa73ebad9b1c944dadb58633cd48d
Arthur <arthur@MacBook-Pro-Arli.local>
2022-06-21 21:32:29 +0200

    minor changes

--- mybooks/books/admin.py
+++ mybooks/books/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Author, Book, Rating
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
 
-    list_display = ["title", "author_forename", "author_surname"]
+    list_display = ["title", "author"]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+
+    list_display = ["forename", "surname"]
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["book", "stars", "rating"]
--- mybooks/books/models.py
+++ mybooks/books/models.py
@@ -1,5 +1,19 @@
 # from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
+
+
+class Author(models.Model):
+    forename = models.CharField(max_length=50, default="")
+    surname = models.CharField(max_length=50, default="")
+
+    def __str__(self):
+        return self.forename + " " + self.surname
+
+    def get_name(self):
+        return self.forename + " " + self.surname
 
 
 class Book(models.Model):
@@ -8,11 +22,49 @@ class Book(models.Model):
     """
 
     title = models.CharField(max_length=100)
-    author_forename = models.CharField(max_length=50, default="")
-    author_surname = models.CharField(max_length=50, default="")
+    subtitle = models.CharField(max_length=100, default="")
+    author = models.ForeignKey(Author, default=None, on_delete=models.CASCADE)
+
+    status_choices = [
+        ("Unread", "Unread"),
+        ("Read", "Read"),
+    ]
+
+    status = models.CharField(
+        max_length=6,
+        choices=status_choices,
+        default="Unread",
+    )
 
     def __str__(self):
         return self.title + " by " + self.get_author()
 
     def get_author(self):
-        return self.author_forename + " " + self.author_surname
+        return self.author.get_name()
+
+
+class Rating(models.Model):
+    """
+    model for rating of book
+    """
+
+    rating = models.TextField(default="")
+    book = models.ForeignKey(Book, default=None, on_delete=models.CASCADE)
+
+    star_choices = [
+        ("0", "0"),
+        ("1", "1"),
+        ("2", "2"),
+        ("3", "3"),
+        ("4", "4"),
+        ("5", "5"),
+    ]
+
+    stars = models.CharField(
+        max_length=1,
+        choices=star_choices,
+        default="1",
+    )
+
+    def __str__(self):
+        return self.rating
